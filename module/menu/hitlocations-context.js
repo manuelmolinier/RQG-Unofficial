11c34f72af023b2a424f65071509653c2419e672
Manuel Molinier <rpgastaroth@gmail.com>
2021-12-04 23:12:11 +0100

    PreUpdate for 0.9

---
+++ module/menu/hitlocations-context.js
@@ -0,0 +1,59 @@
+import { RQGTools } from '../tools/rqgtools.js';
+import { RunequestActorSheet } from '../actor/runequestactor-sheet.js';
+export const hitlocationMenuOptions = (actor, token) => [
+    {
+      name: "Add Wound (soon)",
+      icon: '<i class="fas fa-dice-d20"></i>',
+      condition: () => true,
+      callback: async (el) => {
+        const itemId = RQGTools.getDataset(el, "itemid");
+        const item = actor.items.get(itemId);
+        if (!item || !item.sheet) {
+          const msg = `Couldn't find itemId [${itemId}] on actor ${actor.name} to edit the skill item from the skill context menu`;
+          ui.notifications?.error(msg);
+          throw new Error(msg, el);
+        }
+        item.addWound();
+      }
+    },
+    {
+        name: "Heal Wounds (soon)",
+        icon: '<i class="fas fa-dice-d6"></i>',
+        condition: () => true,
+        callback: async (el) => {
+          const itemId = RQGTools.getDataset(el, "itemid");
+          const item = actor.items.get(itemId);
+          if (!item || !item.sheet) {
+            const msg = `Couldn't find itemId [${itemId}] on actor ${actor.name} to edit the skill item from the skill context menu`;
+            ui.notifications?.error(msg);
+            throw new Error(msg, el);
+          }
+          item.healWound();
+        }
+      },    
+    {
+      name: "Edit",
+      icon: '<i class="fas fa-edit"></i>',
+      condition: () => !!game.user?.isGM,
+      callback: (el) => {
+        const itemId = RQGTools.getDataset(el, "itemid");
+        const item = actor.items.get(itemId);
+        if (!item || !item.sheet) {
+          const msg = `Couldn't find itemId [${itemId}] on actor ${actor.name} to edit the skill item from the skill context menu`;
+          ui.notifications?.error(msg);
+          throw new Error(msg, el);
+        }
+        item.sheet.render(true);
+      }
+    },
+    {
+      name: "Delete",
+      icon: '<i class="fas fa-trash"></i>',
+      condition: () => !!game.user?.isGM,
+      callback: (el) => {
+        const itemId = RQGTools.getDataset(el, "itemid");
+        RunequestActorSheet.confirmItemDelete(actor, itemId);
+      }
+    }
+  ];
+  
